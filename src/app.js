76d64f1590385aaeab2e0f56f482bc9366d41812
Philip Kuryloski <pjk25@cornell.edu>
2016-12-11 10:24:40 +0100

    Refactor

--- src/app.js
+++ src/app.js
@@ -1,36 +1,15 @@
 import Rx from 'rxjs/Rx';
 import m from 'mori';
-import diff from 'virtual-dom/diff';
-import patch from 'virtual-dom/patch';
-import createElement from 'virtual-dom/create-element';
 import render from './render';
+
+import {bootstrap} from './framework_zero'
 
 const initial_state = m.toClj({
   message: 'Framework Zero'
 });
 
-const events = new Rx.BehaviorSubject(m.identity);
-
-const appState = events.asObservable()
-  .scan((state, event) => event(state), initial_state);
-
-appState.scan((uiState, appState) => {
-  let newTree = render(appState);
-  let tree = m.get(uiState, 'tree');
-  let rootNode = m.get(uiState, 'rootNode');
-  if (!rootNode) {
-    rootNode = createElement(newTree);
-    document.body.appendChild(rootNode);
-  } else {
-    let patches = diff(tree, newTree);
-    rootNode = patch(rootNode, patches);
-  }
-  return m.hashMap(
-    'tree', newTree,
-    'rootNode', rootNode
-  );
-}, m.hashMap()).subscribe();
+const eventSink = bootstrap(initial_state, render);
 
 Rx.Observable.of((s) => m.updateIn(s, ['message'], (m) => m + '!'))
   .delay(1000)
-  .subscribe(events);
+  .subscribe(eventSink);
