69c18be911d337b3f1ae9d34a4c5b18e46c48e62
Philip Kuryloski <pjk25@cornell.edu>
2017-02-23 12:34:08 +0100

    Make npm test headless with PhantomJS
    and move the browser based runner to test-watch

--- gulpfile.js
+++ gulpfile.js
@@ -6,28 +6,10 @@ const webpackStream = require('webpack-stream');
 
 gulp.task('jasmine', () => {
     const webpackConfig = webpackMerge(require('./webpack.config'), {
-        entry: './spec/framework_zero_spec.js'
+        entry: ['babel-polyfill', './spec/framework_zero_spec.js']
     });
     return gulp.src([])
         .pipe(webpackStream(webpackConfig, webpack))
         .pipe(jasmineBrowser.specRunner({console: true}))
         .pipe(jasmineBrowser.headless());
-});
-
-
-
-
-
-
-gulp.task('jasmine-browser', () => {
-    const JasminePlugin = require('gulp-jasmine-browser/webpack/jasmine-plugin');
-    const plugin = new JasminePlugin();
-    const webpackConfig = webpackMerge(require('./webpack.config'), {
-        entry: './spec/framework_zero_spec.js',
-        plugins: [plugin]
-    });
-    return gulp.src([])
-        .pipe(webpackStream(webpackConfig, webpack))
-        .pipe(jasmineBrowser.specRunner())
-        .pipe(jasmineBrowser.server({whenReady: plugin.whenReady}));
 });
